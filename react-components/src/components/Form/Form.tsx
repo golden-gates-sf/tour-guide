5a0b5c76c5493b2d8b5ff41be091ef2a7e62c152
Milana Redchenko <redchenko.milana@gmail.com>
2022-11-02 20:48:43 +0300

    feat: add import image

--- react-components/src/components/Form/Form.tsx
+++ react-components/src/components/Form/Form.tsx
@@ -1,5 +1,8 @@
 import React, { RefObject } from 'react';
+import Card from 'components/Card/Card';
 import './form.css';
+import '../../pages/MainPage/mainPage.css';
+import DefaultPic from '../../img/defaultPic.jpg';
 
 const options = [
   { value: 'europe', label: 'Europe' },
@@ -19,63 +22,78 @@ type FormData = {
   picture: string;
 };
 
-class Form extends React.Component {
+type FormProps = Record<string, unknown>;
+interface FormState {
+  isSubmitted: boolean;
+}
+
+class Form extends React.Component<FormProps, FormState> {
   data: FormData;
   titleRef: RefObject<HTMLInputElement>;
   categoriesSelectRef: RefObject<HTMLSelectElement>;
   descriptionRef: RefObject<HTMLTextAreaElement>;
   imageRef: RefObject<HTMLInputElement>;
 
-  constructor(props: Record<string, unknown>) {
+  constructor(props: FormProps) {
     super(props);
 
     this.data = {
-      title: '',
-      categories: [''],
-      description: '',
       picture: '',
+      categories: [''],
+      title: '',
+      description: '',
     };
     this.titleRef = React.createRef();
     this.categoriesSelectRef = React.createRef();
     this.descriptionRef = React.createRef();
     this.imageRef = React.createRef();
+    this.state = {
+      isSubmitted: false,
+    };
   }
 
   render() {
     return (
-      <form
-        className="form-cards"
-        onSubmit={(e) => {
-          e.preventDefault();
-          this.data.title = this.titleRef.current?.value || '';
-          this.data.description = this.descriptionRef.current?.value || '';
-          this.data.picture = this.imageRef.current?.value || '';
-        }}
-      >
-        <label>
-          Title:
-          <input ref={this.titleRef} />
-        </label>
-        <label>
-          Categories:
-          <select size={5} ref={this.categoriesSelectRef} multiple>
-            {options.map((el, i) => (
-              <option value={el.value} key={i}>
-                {el.label}
-              </option>
-            ))}
-          </select>
-        </label>
-        <label>
-          Description:
-          <textarea className="text-description" ref={this.descriptionRef} />
-        </label>
-        <label>
-          Import image:
-          <input type="file" ref={this.imageRef} />
-        </label>
-        <input type="submit" value="Submit" className="btn-submit" />
-      </form>
+      <>
+        <form
+          className="form-cards"
+          onSubmit={(e) => {
+            e.preventDefault();
+            this.data.title = this.titleRef.current?.value || '';
+            this.data.description = this.descriptionRef.current?.value || '';
+            const images = this.imageRef.current?.files || [];
+            images.length > 0
+              ? (this.data.picture = URL.createObjectURL(images[0]))
+              : (this.data.picture = DefaultPic);
+            this.setState({ isSubmitted: true });
+          }}
+        >
+          <label>
+            Title:
+            <input ref={this.titleRef} />
+          </label>
+          <label>
+            Categories:
+            <select size={5} ref={this.categoriesSelectRef} multiple>
+              {options.map((el, i) => (
+                <option value={el.value} key={i}>
+                  {el.label}
+                </option>
+              ))}
+            </select>
+          </label>
+          <label>
+            Description:
+            <textarea className="text-description" ref={this.descriptionRef} />
+          </label>
+          <label>
+            Import image:
+            <input type="file" accept=".png, .jpg, .jpeg" ref={this.imageRef} />
+          </label>
+          <input type="submit" value="Submit" className="btn-submit" />
+        </form>
+        <div className="cards-container">{this.state.isSubmitted && <Card {...this.data} />}</div>
+      </>
     );
   }
 }
--- react-components/src/pages/MainPage/MainPage.tsx
+++ react-components/src/pages/MainPage/MainPage.tsx
@@ -77,7 +77,7 @@ class MainPage extends React.Component {
         <SearchBar />
         <div className="cards-container">
           {cardsArr.map((card, i) => (
-            <Card key={i} {...card} />
+            <Card ref={undefined} key={i} {...card} />
           ))}
         </div>
       </div>
